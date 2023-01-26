2b2b31d6d1bcf10b17decea2f23533013eaa06aa
kaltokri <rolf.geuenich@gmail.com>
2023-01-26 11:31:48 +0100

    First version

---
+++ buildVariations.cmd
@@ -0,0 +1,3 @@
+set filename=ofs-a10c-all-in-one-v076
+
+veaf-tools injectall %filename%.miz %filename%-${version}.miz conf-variations.json
---
+++ conf-var-wind10.lua
@@ -0,0 +1,61 @@
+    ["weather"] = 
+    {
+        ["name_ru"] = "Зима. Oблачность",
+        ["wind"] = 
+        {
+            ["at8000"] = 
+            {
+                ["speed"] = 0,
+                ["dir"] = 0,
+            }, -- end of ["at8000"]
+            ["at2000"] = 
+            {
+                ["speed"] = 0,
+                ["dir"] = 0,
+            }, -- end of ["at2000"]
+            ["atGround"] = 
+            {
+                ["speed"] = 5.154,
+                ["dir"] = 342,
+            }, -- end of ["atGround"]
+        }, -- end of ["wind"]
+        ["enable_fog"] = false,
+        ["season"] = 
+        {
+            ["temperature"] = 20,
+        }, -- end of ["season"]
+        ["qnh"] = 760,
+        ["cyclones"] = 
+        {
+        }, -- end of ["cyclones"]
+        ["dust_density"] = 0,
+        ["enable_dust"] = false,
+        ["clouds"] = 
+        {
+            ["thickness"] = 200,
+            ["density"] = 0,
+            ["preset"] = "Preset1",
+            ["base"] = 4200,
+            ["iprecptns"] = 0,
+        }, -- end of ["clouds"]
+        ["atmosphere_type"] = 0,
+        ["groundTurbulence"] = 0,
+        ["halo"] = 
+        {
+            ["preset"] = "off",
+        }, -- end of ["halo"]
+        ["modifiedTime"] = true,
+        ["name"] = "Winter, clean sky",
+        ["fog"] = 
+        {
+            ["thickness"] = 0,
+            ["visibility"] = 0,
+        }, -- end of ["fog"]
+        ["visibility"] = 
+        {
+            ["distance"] = 80000,
+        }, -- end of ["visibility"]
+        ["name_localize"] = 
+        {
+        }, -- end of ["name_localize"]
+    }, -- end of ["weather"]
